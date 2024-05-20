e31baf6298b817b2df20ff6b301e34dec439cb02
CESAR_ALEXANDER_GONZALEZ_VALDEZ <a.20308051281060@cbtis128.edu.mx>
2024-05-20 16:04:11 +0000

    cambio

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,212 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentButton = 0;
+  List<String> buttonList01 = [
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/oas.jpg",
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/oas.jpg",
+  ];
+  List<String> buttonList02 = [
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/vice.jpg",
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/vice.jpg",
+  ];
+  List<String> buttonList03 = [
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/yo.jpg",
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/yo.jpg",
+  ];
+  List<String> mainList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    mainList = buttonList01;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 2,
+                color: Color(0xffaa10d9),
+              )
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.menu,
+                          color: Color(0xffef0a56),
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.search,
+                          color: Color(0xffe80c3c),
+                        ))
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 16.0, top: 30.0, bottom: 30.0),
+                child: Text(
+                  "iCescMusic",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 38,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: mainList.length + 1,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return RotatedBox(
+                        quarterTurns: 3,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 20, left: 20, right: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    currentButton = 2;
+                                    mainList = buttonList03;
+                                  });
+                                },
+                                child: Text("Yo",
+                                    style: TextStyle(
+                                      color: currentButton == 2
+                                          ? Colors.amber
+                                          : Colors.black,
+                                    )),
+                              ),
+                              SizedBox(
+                                width: 80.0,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    currentButton = 1;
+                                    mainList = buttonList02;
+                                  });
+                                },
+                                child: Text("Viceversa",
+                                    style: TextStyle(
+                                      color: currentButton == 1
+                                          ? Colors.amber
+                                          : Colors.black,
+                                    )),
+                              ),
+                              SizedBox(
+                                width: 100.0,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    currentButton = 0;
+                                    mainList = buttonList01;
+                                  });
+                                },
+                                child: Text(
+                                  "Oasis Album",
+                                  style: TextStyle(
+                                    color: currentButton == 0
+                                        ? Colors.amber
+                                        : Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    } else {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              margin: EdgeInsets.all(20.0),
+                              width: 250,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  child: Image.network(
+                                    mainList[index - 1],
+                                    fit: BoxFit.cover,
+                                  )),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 20),
+                            child:
+                                Text("Oasis", style: TextStyle(fontSize: 26)),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 20, bottom: 26.0),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.circle,
+                                  color: Color(0xffc80d5a),
+                                ),
+                                Text("Compra el exito de jbalvin y badbunny",
+                                    style: TextStyle(
+                                        fontSize: 14, color: Colors.grey))
+                              ],
+                            ),
+                          ),
+                        ],
+                      );
+                    }
+                  },
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
